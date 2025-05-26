b40d96aba36d675c60b03424aa22f31c09e0ea4f
Kayce Basques <kaycebasques@gmail.com>
2025-05-26 06:36:58 -0700

    fix: update the stub type alias names (#2929)
    Co-authored-by: Kayce Basques <kayce@google.com>

--- tools/precompiler/precompiler.py
+++ tools/precompiler/precompiler.py
@@ -68,12 +68,12 @@ def _compile(options: "argparse.Namespace") -> None:
 # A stub type alias for readability.
 # See the Bazel WorkRequest object definition:
 # https://github.com/bazelbuild/bazel/blob/master/src/main/protobuf/worker_protocol.proto
-JsonWorkerRequest = object
+JsonWorkRequest = object
 
 # A stub type alias for readability.
 # See the Bazel WorkResponse object definition:
 # https://github.com/bazelbuild/bazel/blob/master/src/main/protobuf/worker_protocol.proto
-JsonWorkerResponse = object
+JsonWorkResponse = object
 
 
 class _SerialPersistentWorker:
